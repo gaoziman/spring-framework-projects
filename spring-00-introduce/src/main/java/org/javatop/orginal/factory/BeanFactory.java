481aca728c2d9f74907a47fa9cc50446e8a20bf5
gaoziman <2942894660@qq.com>
2024-06-18 06:16:51 +0800

    feat: 改造BeanFactory类，使用双检锁保证beanMap中确实没有beanName对应的对象，避免多线程下重复创建对象。

--- spring-00-introduce/src/main/java/org/javatop/orginal/factory/BeanFactory.java
+++ spring-00-introduce/src/main/java/org/javatop/orginal/factory/BeanFactory.java
@@ -4,6 +4,8 @@ import org.javatop.orginal.dao.DemoDao;
 import org.javatop.orginal.dao.DemoDaoImpl;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Properties;
 
 /**
@@ -15,6 +17,9 @@ import java.util.Properties;
 public class BeanFactory {
 
     private static Properties properties;
+
+    // 缓存区，保存已经创建好的对象
+    private static Map<String, Object> beanMap = new HashMap<>();
 
     // public static DemoDao getDemoDao() {
     //     // return new DemoDaoImpl();
@@ -44,7 +49,7 @@ public class BeanFactory {
     }
 
 
-    public static Object getBean(String beanName) {
+    /*public static Object getBean(String beanName) {
         try {
             // 从properties文件中读取指定name对应类的全限定名，并反射实例化
             Class<?> beanClazz = Class.forName(properties.getProperty(beanName));
@@ -54,5 +59,30 @@ public class BeanFactory {
         } catch (IllegalAccessException | InstantiationException e) {
             throw new RuntimeException("[" + beanName + "] instantiation error!", e);
         }
+    }*/
+
+
+
+    // 改造BeanFactory类，使用双检锁保证beanMap中确实没有beanName对应的对象
+    public static Object getBean(String beanName) {
+        // 双检锁保证beanMap中确实没有beanName对应的对象
+        if (!beanMap.containsKey(beanName)) {
+            synchronized (BeanFactory.class) {
+                if (!beanMap.containsKey(beanName)) {
+                    // 过了双检锁，证明确实没有，可以执行反射创建
+                    try {
+                        Class<?> beanClazz = Class.forName(properties.getProperty(beanName));
+                        Object bean = beanClazz.newInstance();
+                        // 反射创建后放入缓存再返回
+                        beanMap.put(beanName, bean);
+                    } catch (ClassNotFoundException e) {
+                        throw new RuntimeException("BeanFactory have not [" + beanName + "] bean!", e);
+                    } catch (IllegalAccessException | InstantiationException e) {
+                        throw new RuntimeException("[" + beanName + "] instantiation error!", e);
+                    }
+                }
+            }
+        }
+        return beanMap.get(beanName);
     }
 }
